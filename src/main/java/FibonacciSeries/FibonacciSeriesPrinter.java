af42e8acf147aee3324e0976eccc419d3effa9f5
Alexander First <A_First@example.com>
2021-08-24 21:26:50 +0300

    feat: adds FibonacciSeriesPrinter

---
+++ src/main/java/FibonacciSeries/FibonacciSeriesPrinter.java
@@ -0,0 +1,16 @@
+package FibonacciSeries;
+
+public class FibonacciSeriesPrinter {
+
+    public static void printFibonacciSeriesBeforeTo(int beforeValue) {
+        int current = 1;
+        int previous = 0;
+
+        while (current < beforeValue) {
+            System.out.print(current + " ");
+            int tmp = current;
+            current = current + previous;
+            previous = tmp;
+        }
+    }
+}
