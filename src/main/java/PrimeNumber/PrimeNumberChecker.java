2ca3b22b937804ebe6e5afc454865a58fe5025b5
Alexander First <A_First@example.com>
2021-08-25 19:46:03 +0300

    feat: adds PrimeNumberChecker

---
+++ src/main/java/PrimeNumber/PrimeNumberChecker.java
@@ -0,0 +1,16 @@
+package PrimeNumber;
+
+public class PrimeNumberChecker {
+
+    public static boolean isPrimeNumber(int number) {
+        if (number == 2 || number == 3) return true;
+        if (number < 2 || number % 2 == 0) return false;
+        int sqrt = (int) Math.sqrt(number);
+        for (int i = 3; i <= sqrt; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/PrimeNumber/PrimeNumberCheckerTest.java
@@ -0,0 +1,16 @@
+package PrimeNumber;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class PrimeNumberCheckerTest {
+
+    @Test
+    void isPrimeNumber() {
+        assertTrue(PrimeNumberChecker.isPrimeNumber(13));
+        assertFalse(PrimeNumberChecker.isPrimeNumber(15));
+    }
+}
