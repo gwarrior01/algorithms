5c319c472c7558f78aaabdea5b2479d46d862420
Alex First <gwarrior01@mail.ru>
2021-09-19 21:37:47 +0300

    feat: modifies reverse array in place

--- src/main/java/top50/ReverseArray.java
+++ src/main/java/top50/ReverseArray.java
@@ -4,11 +4,26 @@ public class ReverseArray {
 
     public static int[] reverseIterative(int[] inputArray) {
         for (int i = 0; i < inputArray.length / 2; i++) {
-            int tmp = inputArray[i];
-            inputArray[i] = inputArray[inputArray.length - 1 - i];
-            inputArray[inputArray.length - 1 - i] = tmp;
+            swapArrayElements(inputArray, i, inputArray.length - 1 - i);
         }
         return inputArray;
+    }
+
+    private static int[] swapArrayElements(int[] inputArray, int oneElementPosition, int secondElementPosition) {
+        int tmp = inputArray[oneElementPosition];
+        inputArray[oneElementPosition] = inputArray[secondElementPosition];
+        inputArray[secondElementPosition] = tmp;
+        return inputArray;
+    }
+
+    public static int[] reverseRecursive(int[] inputArray) {
+        return reverseRecursive(inputArray, 0, inputArray.length - 1);
+    }
+
+    private static int[] reverseRecursive(int[] inputArray, int leftPos, int rightPos) {
+        if (leftPos >= rightPos) return inputArray;
+        int[] arrayAfterSwap = swapArrayElements(inputArray, leftPos, rightPos);
+        return reverseRecursive(arrayAfterSwap, ++leftPos, --rightPos);
     }
 
 }
