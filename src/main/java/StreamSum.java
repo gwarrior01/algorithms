d73f374ecabcccb2832afbc1da24c39b4c115963
Alex First <gwarrior01@mail.ru>
2021-11-26 21:42:13 +0300

    feat: change the way create stream

--- src/main/java/StreamSum.java
+++ src/main/java/StreamSum.java
@@ -3,13 +3,14 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamSum {
     public static void main(String[] args) {
-        Map<Boolean, Integer> result = Arrays.asList(1, 2, 3, -9, 10, 4, -8, -6, 6).stream()
+        Map<Boolean, Integer> result = Arrays.asList(11, 1, 2, 3, -9, 10, 4, -8, -6, 6, -7).stream()
                 .map(number -> Map.of(number > 0, number))
                 .reduce(
-                        Arrays.asList(true, false).stream().collect(Collectors.toMap(Function.identity(), i -> 0)),
+                        Stream.of(true, false).collect(Collectors.toMap(Function.identity(), i -> 0)),
                         (accumMap, map) -> {
                             Integer number = Optional.ofNullable(map.get(true)).orElse(map.get(false));
                             accumMap.merge(number > 0, map.get(number > 0), (prev, value) -> prev + value);
